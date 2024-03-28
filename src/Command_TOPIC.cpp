5f008a8c39593f7a901f95cf97e46700d36c9d8d
Yono1112 <yuma.ohno.sss@gmail.com>
2024-03-29 05:24:07 +0900

    [fix]: modifiy removeTopicStr() is not executed when arg is "TOPIC #1 :a"
    [body]
    - "TOPIC #1 :a"のように本来であればトピックの設定をするはずが、トピックを削除する処理が実行されてしまっていたため、正しくトピックが設定されるように修正しました
    
    [notes]
    
    close #<issue-number>

--- src/Command_TOPIC.cpp
+++ src/Command_TOPIC.cpp
@@ -28,7 +28,8 @@ void Command::TOPIC(User &user, std::vector<std::string> &arg) {
 	}
 	if (arg.size() == 1) {
 		queryChannelTopic(user, topic_ch);
-	} else if (arg.size() == 2 && arg.at(1)[0] == ':') {
+	} else if (arg.size() == 2 && arg.at(1).size() == 1 &&
+			   arg.at(1)[0] == ':') {
 		removeChannelTopic(user, topic_ch);
 	} else {
 		setChannelTopic(user, topic_ch);
@@ -50,18 +51,10 @@ void Command::queryChannelTopic(User &user, const Channel &topic_ch) {
 }
 
 void Command::removeChannelTopic(User &user, const Channel &topic_ch) {
-	if (topic_ch.getTopicStr() == "") {
-		std::cout << reply_.RPL_NOTOPIC(topic_ch.getName());
-		this->server_.sendMsgToClient(user.getFd(),
-									  reply_.RPL_NOTOPIC(topic_ch.getName()));
-	} else {
-		const_cast<Channel &>(topic_ch).setTopicStr("");
-		std::cout << reply_.RPL_TOPIC(topic_ch.getName(),
-									  topic_ch.getTopicStr());
-		this->server_.sendMsgToClient(
-			user.getFd(),
-			reply_.RPL_TOPIC(topic_ch.getName(), topic_ch.getTopicStr()));
-	}
+	const_cast<Channel &>(topic_ch).setTopicStr("");
+	std::cout << reply_.RPL_NOTOPIC(topic_ch.getName());
+	this->server_.sendMsgToClient(user.getFd(),
+								  reply_.RPL_NOTOPIC(topic_ch.getName()));
 }
 
 void Command::setChannelTopic(User &user, const Channel &topic_ch) {
