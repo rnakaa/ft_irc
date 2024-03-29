2f23165470f9cebb13beddbe80bb65cd9cacde18
rnakaa <102458195+rnakaa@users.noreply.github.com>
2024-03-29 21:32:04 +0900

    Update src/Command_PASS.cpp
    Co-authored-by: Yuma Ohno <100353174+Yono1112@users.noreply.github.com>

--- src/Command_PASS.cpp
+++ src/Command_PASS.cpp
@@ -6,6 +6,8 @@ void Command::PASS(User &user, std::vector<std::string> &arg) {
 		std::cerr << reply_.ERR_ALREADYREGISTRED() << std::endl;
 		server_.sendMsgToClient(user.getFd(), reply_.ERR_ALREADYREGISTRED());
 	} else if (user.getAuthFlags() == User::ALL_AUTH) {
+		std::cerr << reply_.ERR_ALREADYREGISTRED() << std::endl;
+		server_.sendMsgToClient(user.getFd(), reply_.ERR_ALREADYREGISTRED());
 		return;
 	} else if (arg.empty()) {
 		std::cerr << reply_.ERR_NEEDMOREPARAMS("PASS") << std::endl;
